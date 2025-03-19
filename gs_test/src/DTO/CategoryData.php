47dcba442ccf444f54481b7f4d5c577ae3520952
1clogon1 <94410404+1clogon1@users.noreply.github.com>
2025-03-19 16:48:28 +0300

    Update CategoryData.php

--- gs_test/src/DTO/CategoryData.php
+++ gs_test/src/DTO/CategoryData.php
@@ -6,15 +6,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CategoryData
 {
-    /**
-     * @Assert\NotBlank(message="Название категории не может быть пустым.")
-     * @Assert\Length(max=255, maxMessage="Название категории не может превышать 255 символов.")
-     */
-    public $name;
+    #[Assert\NotBlank(message: 'Название категории не может быть пустым.')]
+    #[Assert\Length(max: 255, maxMessage: 'Название категории не может превышать 255 символов.')]
+    public ?string $name = null;
 
-    public function __construct(
-        ?string $name = null
-    )
+    public function __construct(?string $name = null)
     {
         $this->name = $name;
     }
